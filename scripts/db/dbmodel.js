17af1b407e90da795f9ccaeec18a5b9363b1a127
Solo <81814785+sololinux@users.noreply.github.com>
2022-02-09 08:30:26 +0545

    Update dbmodel.js

--- scripts/db/dbmodel.js
+++ scripts/db/dbmodel.js
@@ -24,36 +24,3 @@ const schema = new Schema({
 const db = mongoose.model('users', schema);
 
 module.exports = db;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*| const newu = new user({
-   	email : 'chauraj345r@gmail.com',
-   	username : 'solo2' 
-   })
-
-   newu.save()
-   .then(result =>{
-   	console.log('New user connected');
-   })
-   .catch(err =>{
-   	console.log(err)
-   })
-
-  user.find({username: 'solo3'}, (err,doc) =>{
-   	if(err){console.log(err)}
-   		else{
-   			if(doc == '')
-   			console.log('NO docs')}
-   })*/
